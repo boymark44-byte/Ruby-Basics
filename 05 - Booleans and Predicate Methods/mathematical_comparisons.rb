45ff6a9246d7973ee3c405ddc1644d4fb384aa80
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-08 15:45:22 +0800

    learning curve: Learning the mathematical comparison through symbols whether the either side of the expression is greater than or less than to each other or equal.

---
+++ 05 - Booleans and Predicate Methods/mathematical_comparisons.rb
@@ -0,0 +1,16 @@
+puts 1 < 9 # true
+puts 10 < 5 # false
+puts 1 <= 5 # true
+puts 5 <= 5 # true
+puts 8 <= 5 # false
+
+puts
+
+# Inverse
+puts 10 > 5 # true
+puts -1 > -5 # true
+puts 12 > 20 # false
+puts 8 >= 5 # true
+puts 5 >= 5 # true
+puts 3 >= 5 # false
+
