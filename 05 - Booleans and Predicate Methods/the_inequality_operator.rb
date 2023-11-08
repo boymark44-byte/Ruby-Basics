5f7709f2b8ec4a93cc4a0fbf2da796d3a9cec692
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-08 15:44:09 +0800

    learning curve: Learning the inequality operator that checks whether both sides of the expression are not equal or evaluate to a false return value.

---
+++ 05 - Booleans and Predicate Methods/the_inequality_operator.rb
@@ -0,0 +1,15 @@
+# Inequality Operator "!="
+
+puts 10 != 5 # Returns true
+puts 10 != 10 # Returns false
+
+puts
+
+puts "Hello" != "Goodbye" # true
+puts "Hello" != "hello" # true
+puts "Hello" != "Hello " # true, they have different count of characters, left with 5 and right with 6 characters
+puts "Hello" != "Hello" # false
+
+puts
+
+puts 5 != "5" # true, not the same object or data type
