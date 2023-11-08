e6c99376f4dae093b4b30c007aa95d73177d3ff5
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-08 15:41:55 +0800

    learning curve: Introduction to Ruby Boolean

---
+++ 05 - Booleans and Predicate Methods/intro_to_booleans.rb
@@ -0,0 +1,18 @@
+# boolean
+
+puts true
+puts false
+
+
+puts 5 < 10 # Return a boolean object which is true
+puts 10 > 12 # Return a boolean object which is false
+
+
+# Reference a variable to a boolean object
+handsome = true
+stupid = false
+puts handsome.class # Returns TrueClass
+puts stupid.class # Returns FalseClass
+
+
+
