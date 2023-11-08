dde3f904dd0edcdc29de16f3bec3096151cb34f3
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-08 14:39:34 +0800

    learning curve: Replace the puts method with print method on line 1 and 7 in order to test whether the chomp method works on stripping the spaces and not just the line break

--- 04 - Object Methods/the_gets_method.rb
+++ 04 - Object Methods/the_gets_method.rb
@@ -1,10 +1,10 @@
-puts "Hi, what's your name? "
+print "Hi, what's your name? "
 name = gets.chomp # "Mark Glenn\n", invoking chomp method strips the newline character that is included from the gets method
 
 puts "Your name is #{name}? Nice to meet you!"
 
 
-puts "Great, what's your age?"
+print "Great, what's your age? "
 age = gets.chomp
 
 # We can bypass the Error by using String Interpolation
