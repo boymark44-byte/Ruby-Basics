5c7b5c78b7426334eda155befea110f6624a00c0
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-09 22:24:10 +0800

    Learning Curve: Introduction to how we can define a method or declare a function in Ruby that does not require an argument to work.

---
+++ 06 - Methods/intro_to_methods.rb
@@ -0,0 +1,22 @@
+# Method definition or function definition
+def introduce_myself
+  # Method body or logic body that performs a particular behavior
+  puts "I am handsome!"
+  puts "I am talented!"
+  puts "I am brilliant!"
+end # represents the end of the method
+
+
+# No longer parts of the introduce_myself method but rather from the top-level method
+puts "I am a genius"
+
+
+# Calling the method or invoke the method
+introduce_myself
+puts
+introduce_myself
+
+
+
+
+
