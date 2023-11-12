f613025fac133233eb6ed6100eb4857a7143d3fd
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-12 21:10:57 +0800

    Learning Curve: Learning how to declare a block in Ruby with times method. Knowing the difference between an argument and a block as well as both of them can be used in order to compose a block in Ruby.

---
+++ 07 - Blocks/introduction_to_blocks_with_the_times_method.rb
@@ -0,0 +1,19 @@
+# Argument - object, concrete value
+# Block - a procedure, collection of code
+
+
+# What do we want Ruby to do 5 times? An argument won't solve this problem.
+# This represents a block of procedure.
+5.times { puts "Ruby is awesome!" } # Run this block 5 times
+
+
+puts 
+
+# More than one-line, use "do" and "end"
+value = 3.times do
+  puts "Ruby is awesome!"
+  puts "I'm having fun learning it!"
+end
+
+
+puts value # Returns 3
