a5ce79b598bcab74d97788dabacfdb30d06e2fed
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-13 22:22:10 +0800

    Learning Curve: Introduce and learn how to declare an if-else statement in Ruby and how they works in building conditional logic.

---
+++ 08 - Conditionals/if_and_else.rb
@@ -0,0 +1,23 @@
+# grade = "F" # Just change the value to execute the else block
+
+# if grade == "A"
+#   puts "Mom will be happy"
+# else
+#   puts "Mom will be upset"
+# end
+
+
+puts
+
+
+# Combine if, elsif, and else
+
+grade = "A"
+
+if grade == "A"
+  puts "Mom will be happy"
+elsif grade == "B"
+  puts "Mom will be disappointed but OK"
+else
+  puts "Mom will be upset"
+end
