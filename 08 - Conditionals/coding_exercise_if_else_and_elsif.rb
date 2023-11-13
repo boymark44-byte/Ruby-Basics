bce9981548b8406de0198b8aba1940cbc2cf5328
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-13 22:24:31 +0800

    Coding Exercise: Solve the coding exercise that instructs us to declare a custom method that contains an if-elsif-else statement and a predicate method that determine whether an integer is a positive, negative, and zero.

---
+++ 08 - Conditionals/coding_exercise_if_else_and_elsif.rb
@@ -0,0 +1,88 @@
+# Declare a numeric_energy method that accepts a number.
+# If the number is positive, return the string "Positive"
+# If the number is negative, return the string "Negative"
+# If the number is zero, return the string "Zero Hero"
+#
+# Examples
+# The => indicates the expected return value
+# numeric_energy(5)    => "Positive"
+# numeric_energy(10)   => "Positive"
+# numeric_energy(-5)   => "Negative"
+# numeric_energy(-8)   => "Negative"
+# numeric_energy(0)    => "Zero Hero"
+
+
+# I'm glad that I was able to solve this exercise through predicate method
+def numeric_energy(number)
+  if number.positive?
+    return "Positive"
+  elsif number.negative?
+    return "Negative"
+  else
+    return "Zero Hero"
+  end
+end
+
+p numeric_energy(5)
+p numeric_energy(10)
+p numeric_energy(-5)
+p numeric_energy(-8)
+p numeric_energy(0)
+
+
+puts
+# Another Way of solving it
+def numeric_energy(number)
+  if number > 0
+    return "Positive"
+  elsif number < 0
+    return "Negative"
+  else
+    return "Zero Hero"
+  end
+end
+
+p numeric_energy(5)
+p numeric_energy(10)
+p numeric_energy(-5)
+p numeric_energy(-8)
+p numeric_energy(0)
+
+
+puts
+puts "Author Solution and Result: "
+# Author Solution:
+def numeric_energy(number)
+  if number > 0
+    return "Positive"
+  elsif number < 0
+    return "Negative"
+  else
+    return "Zero Hero"
+  end
+end
+
+p numeric_energy(5)
+p numeric_energy(10)
+p numeric_energy(-5)
+p numeric_energy(-8)
+p numeric_energy(0)
+
+
+puts
+# An optimized solution using Implicit way of returning.
+def numeric_energy(number)
+  if number > 0
+    "Positive"
+  elsif number < 0
+    "Negative"
+  else
+    "Zero Hero"
+  end
+end
+
+p numeric_energy(5)
+p numeric_energy(10)
+p numeric_energy(-5)
+p numeric_energy(-8)
+p numeric_energy(0)
