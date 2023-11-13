1994f805014c55046774914cbcc9e2f060e878be
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-13 22:28:58 +0800

    Learning Curve: Introduce and learn how to add a mutliple condition using the logical or operator.

---
+++ 08 - Conditionals/multiple_conditions_with_or_operator.rb
@@ -0,0 +1,23 @@
+# || or "or" operator, works both
+
+# entree = "Steak"
+# price = 19.99
+
+# if (entree == "Steak") || (price < 29.99)
+#   puts "Atleast one of the conditions is true, purchasing meal"
+# end
+
+
+
+puts
+# Another way of doing this is to assign a variable
+entree = "Steak"
+price = 49.99
+
+# We separate the boolean evaluations to descriptive variables that provide context.
+food_is_delicious = entree == "Steak"
+meal_is_affordable = price < 29.99
+
+if food_is_delicious || meal_is_affordable
+  puts "Atleast one the conditions is true, purchase meal."
+end
