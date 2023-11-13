b5ef1529e242b8c584d513b3c3633d0da663ec49
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-13 22:15:20 +0800

    Learning Curve: Introduce and learn how to declare an if-statement in Ruby.

---
+++ 08 - Conditionals/the_if_statement.rb
@@ -0,0 +1,23 @@
+# if true
+if 5 < 7
+  puts "That math is true."
+end
+
+# if false
+if 5 < 3
+  puts "Something totally different."
+end
+
+password = "topsecret"
+
+if password == "topsecret"
+  puts "Congrats, you've logged in!"
+end
+
+
+word = "kangaroo"
+
+if word.length == 8
+  puts "Hooray, the word has 8 characters"
+  puts "Another line"
+end
