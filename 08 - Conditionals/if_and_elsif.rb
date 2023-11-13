ab9b231fbb8586426e5d6718b2de85f87bf63cc5
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-13 22:21:07 +0800

    Learning Curve: Introduce and learn how to declare an if-elsif statement in Ruby.

---
+++ 08 - Conditionals/if_and_elsif.rb
@@ -0,0 +1,39 @@
+color = "Green" # Just unlock the color if you want to see the other block get executed
+#color = "Yellow" # Just unlock the color if you want to see the other block get executed
+#color = "Red" # Just unlock the color if you want to see the other block get executed
+
+if color == "Red"
+  puts "Red is rad"
+elsif color == "Yellow"
+  puts "Yay for yellow"
+else color == "Green"
+  puts "Green is grape"
+end
+
+
+puts
+
+# Another example, just unlock the number to see other results.
+number = 40
+#number = 10
+#number = 65
+
+if number < 25
+  puts "That's a small number."
+elsif number < 50
+  puts "That's a medium size number."
+elsif number < 75
+  puts "That's a big number."
+elsif number < 150
+  puts "That's a humongous number."
+end
+
+puts
+
+# Syntactically correct but sematically wrong because were comparing two different things.
+# Two pieces of logic that are not related
+if number < 25
+  puts "First condition is true"
+elsif color == "Green"
+  puts "Second condition is true"
+end
