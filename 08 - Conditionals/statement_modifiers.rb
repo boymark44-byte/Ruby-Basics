1368a7de3468c447fb11e903a390a97e9b24c5be
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-15 23:32:18 +0800

    Learning Curve: Introduce and learn how to write a shortcut for and if-statement that has a single-line of logic in a more elegant yet readable way.

---
+++ 08 - Conditionals/statement_modifiers.rb
@@ -0,0 +1,37 @@
+number = 10_0000
+
+# Common way of writing an if-statement with one-line of logic between the if and the end
+if number > 5_000
+  puts "That's a huge number!"
+end
+
+
+
+puts
+# Alternative way of writing with statement modifiers with only one line
+# Elegant way of writing an if-statement
+# Syntaxt is move the execution line or body, then, followed by the single or multiple condition statement
+puts "That's a huge number!" if number > 5_000
+
+
+
+puts
+# Same rules apply if we have multiple conditions
+number = 10_0000
+verified = true
+
+puts "That's a huge number!" if number > 5_000 && verified
+
+
+
+puts
+# With unless statement
+another_number = 8
+
+unless another_number > 10 # Execute if false
+  puts "another_number is NOT greater than 10"
+end
+
+puts
+# Rewriting the code above using statement modifier
+puts "another_number is NOT greater than 10" unless another_number > 10 
