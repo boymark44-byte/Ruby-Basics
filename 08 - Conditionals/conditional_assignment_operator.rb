93e21792604186d1e114b2a58f183212c283fb14
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-15 23:34:42 +0800

    Learning Curve: Introduce and learn how to utilize a particular operation in Ruby which is the conditional assignement operator that works like an if-statement due to its operation need a boolen value to execute. It is also a helpful technique that can save developers computing resources by not repeating a particular expensive operation by simply storing and accessing its value from a given variable.

---
+++ 08 - Conditionals/conditional_assignment_operator.rb
@@ -0,0 +1,20 @@
+# Written with two vertical pipes along with an assignment operator: ||=
+
+# Conditional Assignment was written to assign a value to a variable very similar to how the equal sign works but the difference here is it is conditional meaning that Ruby will only assign that value to a variable based on a condition or criteria being met.
+# And that criteria is if the variable's current value is nil.
+
+# What this operator does is that if the current value of a variable is nil,
+# it assigns a new value to that variable.
+# And if not, it does nothing.
+
+
+my_value = nil
+p my_value
+
+
+my_value ||= 5
+p my_value
+
+
+my_value ||= 10
+p my_value 
