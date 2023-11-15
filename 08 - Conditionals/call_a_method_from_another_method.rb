77fb1ff4db84811e8569476ef997423a9814b74c
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-15 23:28:38 +0800

    Learning Curve: Introduce and learn how to do a fragmented approach by breaking up the logic into smaller pieces and then invoke them from a custom method. Basically isolating a particular logic and then calling them.

---
+++ 08 - Conditionals/call_a_method_from_another_method.rb
@@ -0,0 +1,39 @@
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def divide(a, b)
+  a / b
+end
+
+
+def calculator(a, b, operation)
+  if operation == "add"
+    # Invoking the add method from the body of my calculator method
+    add(a, b)
+  elsif operation == "subtract"
+    subtract(a, b)
+  elsif operation == "multiply"
+    multiply(a, b)
+  elsif operation == "divide"
+    divide(a, b)
+  else
+    "That's not an available math operation, genius!"
+  end
+
+end
+
+# Calling the main body method calculator
+puts calculator(3, 5, "add")
+puts calculator(2, 5, "subtract")
+puts calculator(3, 5, "multiply")
+puts calculator(20, 4, "divide")
+
