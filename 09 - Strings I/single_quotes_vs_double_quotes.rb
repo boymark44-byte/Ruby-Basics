0331e7f9b784363c47d963f5c688242c10ed6f72
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-16 21:38:51 +0800

    learning curve: Introduce and learn how to use single and double quotes when creating a string as well as determine the difference between them.

---
+++ 09 - Strings I/single_quotes_vs_double_quotes.rb
@@ -0,0 +1,15 @@
+# Can be decalre using single or double quotes
+puts "hello\n\n\nworld"
+puts 'hello\n\n\nworld' # Ruby is a lot more literal
+
+
+# C:\Documents\news
+puts 'C:\Documents\news' # Taking every single-character literally, not allowing the to escape
+
+
+puts
+phrase = "Hello world"
+puts "My phrase is #{phrase}"
+puts 'My phrase is #{phrase}'
+
+
