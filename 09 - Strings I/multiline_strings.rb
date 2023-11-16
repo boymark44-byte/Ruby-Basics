0570f0d5e77d7cc724c8f75392533a355989359c
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-16 21:40:19 +0800

    learning curve: Introduce and learn how to declare a multi-line string which is a very useful operator in Ruby that allows us to input string in an identifier that occupies multiple lines in our code.

---
+++ 09 - Strings I/multiline_strings.rb
@@ -0,0 +1,11 @@
+        # The beginning of the multi-line string
+poetry = <<MLS
+  What a beautiful language Ruby is
+  It allows you to write multiple lines
+  Split your words up across mutliple lines
+  Everything still looks nice and fine.
+  - Shakespeare
+MLS
+# The end.
+
+puts poetry
