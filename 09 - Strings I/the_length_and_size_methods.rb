d244eb6dad2223ce1daf7dd4f79395b76fb78595
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-16 21:46:03 +0800

    learning curve: Introduce and learn how to obtain the length or number of characters that a given string possess.

---
+++ 09 - Strings I/the_length_and_size_methods.rb
@@ -0,0 +1,12 @@
+puts "hello world".length
+puts "   # # $ $@$ @#  ".length
+puts "".length
+
+
+puts
+puts "hello world".size # Another name for length method
+puts "   # # $ $@$ @#  ".size # Another name for length method
+puts "".size # Another name for length method
+
+
+puts
