1b18deadcbbe2a74a65169096759703f788d517c
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-17 17:20:08 +0800

    learning curve: Introduce and learn how to overwrite and replace an existing character in a string in Ruby using the same convention of accessing them through their index first and then the set of characters you want to store.

---
+++ 09 - Strings I/overwrite_string_characters.rb
@@ -0,0 +1,19 @@
+thing = "rocket ship"
+
+# Replace the character from index 0 with a new character
+thing[0] = "p"
+puts thing
+
+thing[1] = "a"
+puts thing
+
+thing[9] = "o"
+puts thing
+
+
+puts # Replacing character using multiple-line
+fact = "I love blueberry pie" # replace with rasp
+#fact[7, 4] = "rasp"
+#fact[7, 4] = "cherry"  # No problem with a long replacement
+fact[7, 4] = "red"      # No problem with a short replacement
+puts fact
