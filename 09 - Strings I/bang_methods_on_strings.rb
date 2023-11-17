cc06d5c64ec332b6ad3dbc31d4d3b5e98b0a7641
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-17 17:49:35 +0800

    learning curve: Introduce and learn how to utilize the bang-method! which is a method similar to how a predicate method works but alter or modify the original object.

---
+++ 09 - Strings I/bang_methods_on_strings.rb
@@ -0,0 +1,35 @@
+# bang methods end with a !
+# bang methods peform some kind of mutation, modify, or change an object that the method is invoke upon.
+
+
+word = "spaghetti" # original object
+puts word.capitalize # Wont alter the original object\
+puts word
+
+
+# ====================================================== #
+
+
+# Using bang-method! will alter or mutate the object it is invoked upon.
+puts
+word = "spaghetti"
+puts word.capitalize!
+puts word # Altered object
+
+puts
+word.upcase!
+puts word
+
+puts
+word.downcase!
+puts word
+
+puts
+word.reverse!
+puts word
+
+puts
+word.swapcase! # Swapping from downcase to upcase and vice-versa
+puts word
+
+
