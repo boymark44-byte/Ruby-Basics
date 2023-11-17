ac25d5ac7ebcf5de8642950c8195ab4d2a6c2bab
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-17 17:43:38 +0800

    learning curve: Introduce and learn how to utilize swapcase method to invert the original case of the string from upcase to downcase and vice versa.

---
+++ 09 - Strings I/case_methods.rb
@@ -0,0 +1,15 @@
+# The upcase, downcase, and swapcase method.
+puts "Eiffel Tower".upcase
+puts "Eiffel Tower".downcase
+puts "Eiffel Tower".swapcase # Swaps the current case our string is in and returns a new one that is an inverted version of the original string
+
+
+puts # The capitalize method this method returns a new string with the first letter being capitalized.
+puts "eiffel tower".capitalize
+puts "EIFFEL TOWER".capitalize # Not effective if the string is already capitalized
+
+
+puts # None of these methods mutate or modify the original string, rather they return a new one
+tourist_hotspot = "Eiffel Tower"
+puts uppercased_hotspot = tourist_hotspot.upcase
+puts tourist_hotspot # Did not mutate the string but rather return a new one 
