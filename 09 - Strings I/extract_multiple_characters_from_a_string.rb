c49a9a1b58680aa3d19d98f4216ac711bbb96010
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-17 17:18:09 +0800

    learning curve: Introduce and learn how to extract multiple characters from a string using the traditional square bracket method and slice method that takes an index where to start to extraction process and what characters to extract.

---
+++ 09 - Strings I/extract_multiple_characters_from_a_string.rb
@@ -0,0 +1,15 @@
+story = "Once upon a time in a land far, far away..."
+
+# story[start, no. of characters to pull-out]
+puts story[5, 4]
+puts story[0, 5]
+puts story[0, story.length] # Extracting the whole string in 2nd argument
+puts story[-7, 5] # Starting from the end
+
+
+puts # Using slice method, another way to extract character using index, a method-based approach.
+puts story.slice(5, 4)
+puts story.slice(0, 5)
+puts story.slice(0, story.length) # Extracting the whole string in 2nd argument
+puts story.slice(-7, 5) # Starting from the en
+
