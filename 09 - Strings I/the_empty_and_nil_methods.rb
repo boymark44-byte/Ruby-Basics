80afc89ae9684e1629c182d9d4cceb6b36012872
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-17 17:25:31 +0800

    learning curve: Introduce and learn how to invoke the predicate method empty and nil to identify whether a particular object contains characters and whether a particular object is a nil object which represents absence or nothingness. Both of them returns a Boolean value.

---
+++ 09 - Strings I/the_empty_and_nil_methods.rb
@@ -0,0 +1,15 @@
+# The empty methods checks whether a given string is empty or no existing characters.
+puts "".empty?
+puts "abc".empty?
+puts "".empty?
+
+
+# The nil method
+puts "hello".nil?
+puts 5.nil?
+puts
+something = nil
+puts something.nil?
+
+
+
