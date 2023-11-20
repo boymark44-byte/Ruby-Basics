06c10b063540d85f8c64aca71fbab68f7a6931ee
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-20 23:38:52 +0800

    learning curve: Introduce and learn how to implement the until loop that works like while loop but in complete opposite or the inverse of it. It automatically execute its logic body while the given condition is false, otherwise it stops when it becomes true.

---
+++ 10 - Loops/the_until_loop.rb
@@ -0,0 +1,13 @@
+# A while loop executes while a condition is true
+# An until loop executes until a condition is true
+
+i = 1
+until i > 9 # until i > 9, keep running the logic
+  puts i
+
+  i += 1
+end
+
+puts
+puts i # The final value of i
+
