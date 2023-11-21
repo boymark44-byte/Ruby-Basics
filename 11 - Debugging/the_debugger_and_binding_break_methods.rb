378b82f7d2255510dcee121773bcc7b72726f92d
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-21 17:29:00 +0800

    learning-curve: Introduce and learn how to utilize the debug gem from the Standard Library of Ruby. Invoke and use the debugger and binding.break method to pause or halt the program at that particular moment of execution and understand the underlying process that the program is doing.

---
+++ 11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,15 @@
+require "debug"
+
+# Standard Library - collection of code organized into libraries
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+binding.break
+
+beverage = "Arizona Iced Tea"
+puts "I love drinking #{beverage}"
+
+binding.break 
+
+puts "This is the end of the program."
