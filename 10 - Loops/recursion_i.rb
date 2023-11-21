793196f3558f28de6d210452c061ba32d55cc071
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-21 15:06:25 +0800

    doc: Added a documenation to the 1st recursive function we encounter for clarity.

--- 10 - Loops/recursion_i.rb
+++ 10 - Loops/recursion_i.rb
@@ -15,13 +15,19 @@
 # 2! = 2 * 1            = 2
 # 1!                    = 1
 
+# This function calculates the factorial of a given number recursively
 def factorial(number)
-  return 1 if number == 1 # Base Case and we are using statement modifier to shortened the if-statement
+  # Base case: if number is 1, return 1
+  return 1 if number == 1
+
+  # Recursive case: multiply the number with the factorial of (number - 1)
   number * factorial(number - 1)
 end
 
+# Test the function with two inputs and print the output
 puts factorial(5) # Returns 120
 puts factorial(6) # Returns 720
+
 
 # The underlying process for factorial(5):
 # First Layer: 5 * factorial(4)
