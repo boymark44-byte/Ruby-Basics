9a97fb067c6f8b9498fa899a8c334ac6440221e6
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-21 23:36:10 +0800

    learning-curve: Introduce and learn how to declare an array and store elements within it.

---
+++ 12 - Arrays I/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+# How to declare an empty array => array[]
+
+# An array with 6 elements
+numbers = [4, 8, 15, 16, 23, 42]
+p numbers # Use p for more technical output than the puts method
+
+
+# A valid Ruby code but not advisable due to elements having different types
+things = [4, true, "Hello", 10.99]
+p things
+
+
+# Ideally, use array when storing elements of same types
+toys = ["Teddy Bear", "Water Gun", "Board Game"]
+p toys
+
+registrations = [true, true, false, true, false]
+p registrations
+
