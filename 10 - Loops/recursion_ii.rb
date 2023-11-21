32c210d76ed4dd7b7b178d85acbd6f220a7903fc
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-21 15:07:25 +0800

    doc: Added a documentation to the 2nd recursive function which reverses a string. The added documentation allows us the gain clarity on how the method works recursively.

--- 10 - Loops/recursion_ii.rb
+++ 10 - Loops/recursion_ii.rb
@@ -1,21 +1,26 @@
 #puts "straw".reverse
 
-# Creating our own reverse method with iteration-based solution
-# def reverse(text)
-#   first_index = 0
-#   last_index = text.length - 1
-#   reversed_text = ""
+# Creating our own reverse method with iteration-based solution.
+# The following code defines a function that takes a string as an argument and returns the reverse of that string.
+# The function uses a while loop to iterate through the string from the last character to the first character.
+# It then concatenates each character to a new string, which is returned as the reversed string. The code is as follows:
 
 
-#   while last_index >= first_index # 4 >= 0
-#     reversed_text += text[last_index] # w + a + r + t + s
-#     last_index -= 1 # Decrement
-#   end
+def reverse(text)
+  first_index = 0 # set the first index to 0
+  last_index = text.length - 1 # set the last index to the length of the string minus 1
+  reversed_text = "" # initialize an empty string to store the reversed text
 
-#   reversed_text
-# end
+  while last_index >= first_index # while the last index is greater than or equal to the first index
+    reversed_text += text[last_index] # add the character at the last index to the reversed text string
+    last_index -= 1 # decrement the last index
+  end
 
-# puts reverse("straw")
+  reversed_text # return the reversed text string
+end
+
+puts reverse("straw") # prints "warts"
+
 
 
 puts
@@ -23,11 +28,16 @@ puts
 # ================================================================= #
 # Creating our own reverse method with recursion-based solution
 
+# This function takes a string as an argument and returns the string in reverse order using recursion
 def reverse(text)
-  return text if text.length == 1 # Our Base Case "s" that forces the recursion to stop
-  last_character = text[-1] # w
-  remainder_string = text[0, text.length - 1] # text[0, 4] pulling out all the charaters except for the last one
-  last_character + reverse(remainder_string) # w + reverse("stra")
+  # Base of the string is 1, return the string itself
+  return text if text.length == 1
+  # Assign the last character of the string to the variable last_character
+  last_character = text[-1]
+  # Assign the remainder of the string (excluding the last character) to the variable remainder_string
+  remainder_string = text[0, text.length - 1]
+  # Recursively call the reverse function with the remainder_string as the argument and concatenate the last_character
+  last_character + reverse(remainder_string)
 end
 
 # The underlying process:
