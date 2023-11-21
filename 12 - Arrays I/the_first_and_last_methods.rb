18ae0344979202b7cacae7c69307ccb96bc7cfe2
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-21 23:47:21 +0800

    learning-curve: Introduce and learn how to utilize two array methods namely first and last which allows us toaccess the first elements of an array just like how we use bracket[0] and vice-versa with the last method. If provided with argument, we will be guranteed with a return value of an array, otherwise we will be returned with a string or the default object of the element.

---
+++ 12 - Arrays I/the_first_and_last_methods.rb
@@ -0,0 +1,14 @@
+sushi = ["Salmon", "Tuna", "Shrimp", "Eel", "Yellowtail"]
+
+puts # The first method
+p sushi.first # Returns the first element of an array in string form
+p sushi.first(4) # Returns the first-four of an array
+p sushi.first(3) # Returns the first-three of an array
+p sushi.first(1) # Returns the first-three of an array
+
+
+puts # The last method
+p sushi.last
+p sushi.last(4)
+p sushi.last(3)
+p sushi.last(1)
