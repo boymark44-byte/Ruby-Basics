d777964623673bd0f686857c08f89838d059e62b
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-22 21:14:22 +0800

    learning-curve: Introduce and learn how to use pop method in popping or pulling an element from the array.

---
+++ 12 - Arrays I/the_pop_method.rb
@@ -0,0 +1,22 @@
+bubble_tea_flavors = [
+  "Chocolate",
+  "Kiwi",
+  "Peach",
+  "Plum",
+  "Strawberry",
+  "Taro"
+]
+
+# Invoking pop without an argument
+p bubble_tea_flavors.pop # Removes the specified element and returns it
+p bubble_tea_flavors
+
+
+puts # Invoking pop method with an argument
+p bubble_tea_flavors.pop(1)
+p bubble_tea_flavors
+
+
+puts #
+p bubble_tea_flavors.pop(2)
+p bubble_tea_flavors
