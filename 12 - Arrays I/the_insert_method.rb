bd546b8f1764d51c05ca9acfd81976aaedcb7a78
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-22 21:11:25 +0800

    learning-curve: Introduce and learn how to insert an element to an array.

---
+++ 12 - Arrays I/the_insert_method.rb
@@ -0,0 +1,9 @@
+credit_card_transactions = [19.99, 29.43, 3.87]
+
+# insert(index position, value to insert)
+credit_card_transactions.insert(1, 49.99)
+
+credit_card_transactions.insert(0, 1.23, 4.46, 7.89) # Inserting multiple values
+
+
+p [1, 2, 3].insert(8, 1000)
