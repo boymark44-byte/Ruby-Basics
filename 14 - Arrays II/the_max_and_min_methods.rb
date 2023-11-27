a13202a531c28936e794e3b2e0b4bda3b201de78
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-27 22:44:46 +0800

    learning-path: Introduce and learn how to utilize the min and max method to determine whether a particular element in an array is the least value if they are close to the beginning of the alphabet, and the greatest value if they are close to the end of the alphabet.

---
+++ 14 - Arrays II/the_max_and_min_methods.rb
@@ -0,0 +1,18 @@
+# max - greatest value in an array
+# min - smalles value in an array
+
+
+stock_prices = [434.12, 723.99, 84.12, 649.92]
+p stock_prices.min # Returns the minimum value
+p stock_prices.max # Returns the maximum value
+
+
+puts # With strings
+companies = ["Nike", "Microsft", "Apple", "Google"]
+p companies.max
+p companies.min
+
+p [].max # Returns nil
+p [].min # Returns nil
+
+
