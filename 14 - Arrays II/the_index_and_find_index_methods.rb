2c31c28ca79688562665a4ad2c333c5bc11678d5
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-27 22:39:33 +0800

    learning-path: Introduce and learn how to use the index and find_index method in returning the given index position of an element in an array.

---
+++ 14 - Arrays II/the_index_and_find_index_methods.rb
@@ -0,0 +1,7 @@
+# index/find_index - return the index position of the first occurrence of element
+
+colors = ["Red", "Blue", "Green", "Red"]
+
+p colors.index("Green") # Returns 2
+p colors.index("Red") # Returns 0
+p colors.index("Orange") # Returns nil
