8d0afc236ebdbb8a1b376e5935b49c0bfd920970
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-27 23:05:25 +0800

    coding-exercise: Solve the coding exercise that instructs us to implement the unlimited way of adding arguments to a method call.

---
+++ 14 - Arrays II/coding_exercise_unlimited_method_arguments.rb
@@ -0,0 +1,23 @@
+# Define a sum_of_string_lengths method that accepts
+# any number of strings. The method return the sum
+# of the lengths of the strings.
+#
+# Examples:
+# The => indicates the expected return value
+# sum_of_string_lengths("bob", "loves", "burgers")     => 15
+# sum_of_string_lengths("coding", "is", "so", "fun")   => 13
+# sum_of_string_lengths()                              => 0
+
+
+def sum_of_string_lengths(*strings)
+  rolling_sum = 0
+
+  strings.each do |string|
+    rolling_sum = rolling_sum + string.length
+  end
+
+  rolling_sum
+end
+p sum_of_string_lengths("bob", "loves", "burgers")
+p sum_of_string_lengths("coding", "is", "so", "fun")
+p sum_of_string_lengths()
