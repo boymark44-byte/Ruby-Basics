4b7f799719a327376905b508078bb63161191d8b
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-27 22:33:53 +0800

    learning-path: Introduce and learn how to use the select and reject method in an array on what to include and reject given that they satisfy a condition.

---
+++ 14 - Arrays II/the_select_and_reject_methods.rb
@@ -0,0 +1,16 @@
+# select - filter array for elements that satisfy a condition
+# reject - filter array for elements that do not satisfy a condition
+
+
+words = ["racecar", "selfless", "senteces", "level"]
+# palindrome - a word that is the same backwards as it is forwards
+palindromes = words.select { |word| word == word.reverse } # racecar == racecar which is true
+p palindromes
+
+
+animals = ["cheetah", "cat", "lion", "elephant", "dog", "cow"]
+# If the block returns true, then Ruby will REJECT that element (Exclude it)
+# IF the block returns false, then Ruby will INCLUDE that element (Include it)
+p animals.reject { |animal| animal.include?("c") }
+
+
