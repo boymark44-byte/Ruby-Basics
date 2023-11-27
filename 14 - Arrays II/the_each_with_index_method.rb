4e06ff6a9f6822f65e29601c00daf4114b1d5707
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-27 20:16:44 +0800

    learning-path: Introduce and learn how to use the each_with_index method that iterates over array elements along with its index positions in an array in Ruby.

---
+++ 14 - Arrays II/the_each_with_index_method.rb
@@ -0,0 +1,12 @@
+# each_with_index
+
+colors = ["Red", "Blue", "Green", "Yellow"]
+
+colors.each_with_index { |color, index| puts "The value for index position #{index} is #{color}" }
+
+
+puts # with multiple-line block
+[5, 10, 15].each_with_index do |number, index|
+  puts "The number is #{number} and the index is #{index}"
+  puts "The product of the two is #{number * index}"
+end
