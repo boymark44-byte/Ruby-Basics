0ec3c996d4213a7ff45c601a3eea9c486c36c7dd
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-28 10:54:49 +0800

    coding-exercise: Added the instructor solution in the Ruby file.

--- 14 - Arrays II/coding_exercise_unlimited_method_arguments.rb
+++ 14 - Arrays II/coding_exercise_unlimited_method_arguments.rb
@@ -8,7 +8,6 @@
 # sum_of_string_lengths("coding", "is", "so", "fun")   => 13
 # sum_of_string_lengths()                              => 0
 
-
 def sum_of_string_lengths(*strings)
   rolling_sum = 0
 
@@ -18,6 +17,20 @@ def sum_of_string_lengths(*strings)
 
   rolling_sum
 end
+p sum_of_string_lengths("bob", "loves", "burgers")
+p sum_of_string_lengths("coding", "is", "so", "fun")
+p sum_of_string_lengths()
+
+
+
+puts # Instructor Solution:
+
+def sum_of_string_lengths(*elements)
+  total = 0
+  elements.each { |element| total += element.length }
+  total
+end
+
 p sum_of_string_lengths("bob", "loves", "burgers")
 p sum_of_string_lengths("coding", "is", "so", "fun")
 p sum_of_string_lengths()
