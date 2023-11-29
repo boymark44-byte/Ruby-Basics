6ead49c00c77913bf3f31aef78953a09c620068d
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-29 22:30:47 +0800

    learning-path: Introduce and learn how to use the join method when combining strings within an array in Ruby.

---
+++ 15 - Strings II/the_join_method_on_an_array.rb
@@ -0,0 +1,18 @@
+# join method - merge array elements into a string {optionally using a delimiter}
+
+registrants = ["Jake", "Mikey", "Hannah"]
+
+puts # Join them together without using a delimiter
+p registrants.join
+
+
+puts # Join them together using a (-) delimiter
+p registrants.join("-")
+
+
+puts
+p registrants.join("!*!")
+
+
+puts
+p registrants.join(", ")
