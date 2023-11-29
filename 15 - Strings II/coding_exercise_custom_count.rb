7ca83db3dfc3eff2eff4e90adc7c06ecae3ac6b2
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-29 22:51:55 +0800

    coding-exercise: Solve and submit a coding exercise that instructs to implement a custom count method that counts the number of times a character appears on a given string.

---
+++ 15 - Strings II/coding_exercise_custom_count.rb
@@ -0,0 +1,29 @@
+# Define a custom_count method that accepts a string and a string
+# of search characters. The method should count how many times the
+# search characters appear in the original string. Do not use the
+# built-in count method in your solution.
+#
+# Examples:
+# The => indicates the expected return value
+# custom_count("Hello World", "l")     => 3
+# custom_count("Hello World", "O")     => 0
+# custom_count("Hello World", "z")     => 0
+# custom_count("Hello World", "lo")    => 5
+# custom_count("Hello World", "ol")    => 5
+def custom_count(strings, search_char)
+  count = 0
+
+  # Iterate through each character in the original string
+  strings.each_char do |character|
+    # Check if the character is present in the search characters
+    count += 1 if search_char.include?(character)
+  end
+
+  count
+end
+
+p custom_count("Hello World", "l")
+p custom_count("Hello World", "O")
+p custom_count("Hello World", "z")
+p custom_count("Hello World", "lo")
+p custom_count("Hello World", "ol")
