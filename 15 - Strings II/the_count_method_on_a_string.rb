3d7cd41dff971c33665d1fc08ae8d3dcdef4635e
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-11-29 22:32:11 +0800

    learning-path: Introduce and learn how to use the count method on counting the number of occurrences of a character or string in a string(s).

---
+++ 15 - Strings II/the_count_method_on_a_string.rb
@@ -0,0 +1,6 @@
+# count method - count the number of occurrences of character(s) in a string
+
+puts "Hello World".count("l")
+puts "Hello World".count("w")
+puts "Hello World".count("lo") # l is 3 and o is 2 => 5
+puts "Hello World".count("WL") 
