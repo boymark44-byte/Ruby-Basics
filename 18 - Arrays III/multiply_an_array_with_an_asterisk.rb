7af9fc7561b3b4c8715351389b5dca0afe6139cf
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2025-01-14 15:14:02 +0800

    We learned how to multiply array elements using an asterisk or the multiplication operator also known as asterisk.

---
+++ 18 - Arrays III/multiply_an_array_with_an_asterisk.rb
@@ -0,0 +1,12 @@
+#* Multiplying an array using the asterisk "*" or multiplication operator.
+
+
+puts 4 * 3 # => 
+
+puts "Ruby" * 3 # => 
+
+p [1, 2, 3] * 5  # => [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+p ["Mark", "Glenn", "Gabuya"] * 2 # => ["Mark", "Glenn", "Gabuya", "Mark", "Glenn", "Gabuya"]
+
+
